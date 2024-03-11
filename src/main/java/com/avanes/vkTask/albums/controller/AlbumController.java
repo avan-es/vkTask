258a357174772d8b308e213118d7c45c7776a971
avan-es <bagratavanesian@yandex.ru>
2024-03-11 18:22:52 +0300

    Добавил CRUD операции, валидацию и PreAuthorize

--- src/main/java/com/avanes/vkTask/albums/controller/AlbumController.java
+++ src/main/java/com/avanes/vkTask/albums/controller/AlbumController.java
@@ -1,36 +1,65 @@
 package com.avanes.vkTask.albums.controller;
 
-import com.avanes.vkTask.albums.POJO.Albums;
-import com.avanes.vkTask.albums.POJO.AlbumsPost;
+import com.avanes.vkTask.albums.POJO.AlbumFull;
+import com.avanes.vkTask.albums.POJO.Album;
 import com.avanes.vkTask.albums.service.AlbumService;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/albums")
+@RequestMapping("avan/albums")
+@Validated
+@Slf4j
 public class AlbumController {
 
     private final AlbumService albumService;
 
+
     @GetMapping("/{id}")
-    public Albums getAlbumById(@PathVariable Integer id) {
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ALBUMS')")
+    @ResponseStatus(HttpStatus.OK)
+    public AlbumFull getAlbumById(
+            @PositiveOrZero @PathVariable Long id) {
         return albumService.getAlbum(id);
     }
 
     @GetMapping()
-    public Albums[] getAlbums() {
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ALBUMS')")
+    @ResponseStatus(HttpStatus.OK)
+    public AlbumFull[] getAlbums() {
         return albumService.getAlbums();
     }
 
     @PostMapping
-    public Albums addAlbum(@RequestBody AlbumsPost album) {
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ALBUMS')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Album/*AlbumFull*/ addAlbum(
+            @RequestBody Album album) {
         return albumService.addAlbum(album);
     }
+
     @PatchMapping("/{id}")
-    public Albums patchAlbum(@RequestBody AlbumsPost album,
-                             @PathVariable Integer id) {
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ALBUMS')")
+    @ResponseStatus(HttpStatus.OK)
+    public AlbumFull patchAlbum(
+            @RequestBody Album album,
+            @PositiveOrZero @PathVariable Long id) {
         return albumService.patchAlbum(album, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ALBUMS')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAlbum(
+            @PositiveOrZero @PathVariable Long id) {
+        albumService.deleteAlbum(id);
     }
 
 }
