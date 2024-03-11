cfa18b1f63dcbbb9b763cbb4dfdf4e86c288608c
avan-es <bagratavanesian@yandex.ru>
2024-03-11 21:31:18 +0300

    Класс для проверки ролей

---
+++ src/main/java/com/avanes/vkTask/security/GetAuthUserLogin.java
@@ -0,0 +1,27 @@
+package com.avanes.vkTask.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum GetAuthUserLogin {
+    INSTANCE;
+
+    public Map<String, String> getAuthUserLogin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        Set<String> roles = authentication.getAuthorities().stream()
+                .map(r -> r.getAuthority()).collect(Collectors.toSet());
+        HashMap<String, String> result = new HashMap<>();
+        StringBuilder totalRoles = new StringBuilder();
+        for (String role : roles) {
+            totalRoles.append(role).append(" ");
+        }
+        result.put(currentPrincipalName, totalRoles.toString().trim());
+        return result;
+    }
+}
