deaf032487950d709ab3f3433c4c6247905f2e65
avan-es <bagratavanesian@yandex.ru>
2024-03-11 23:39:29 +0300

    Рефакторинг

--- src/main/java/com/avanes/vkTask/security/SecurityConfig.java
+++ src/main/java/com/avanes/vkTask/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.avanes.vkTask.security;
 
 
-import com.avanes.vkTask.users.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
